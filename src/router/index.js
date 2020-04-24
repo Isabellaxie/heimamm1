e43932c2e5d028f6e3357f1fb11964e55c5719e4
Isabellaxie <58657425+Isabellaxie@users.noreply.github.com>
2020-04-24 23:32:23 +0800

    删除测试用的son

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  import Vue from 'vue'
  // 导入 vue-router
 import VueRouter from 'vue-router'
-import son from '@/components/son.vue'
+import login from '@/views/login.vue'
 //注册 vue-router
 Vue.use(VueRouter)
 // 实例化
@@ -10,8 +10,8 @@ const router = new VueRouter({
     //这里就是路由的配制项
     routes: [
         {
-            path: '/son',
-            component:son  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
+            path: '/',
+            component:login  //这里要填入一个组件名(填入import的名字)，也就是上面地址对应的组件
  }
     ]
 })
